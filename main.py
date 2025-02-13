e02b744ec70d6e5df400331e4447d1ae3dbd2c6e
Clarets614 <cstock614@gmail.com>
2025-02-13 18:21:34 -0500

    main function created and .gitignore file added

---
+++ main.py
@@ -0,0 +1,10 @@
+path_to_file = "books/frankenstein.txt"
+
+    
+def main():
+    path_to_file = "books/frankenstein.txt"
+    with open(path_to_file) as f:
+      file_contents = f.read()
+    print(file_contents)  
+
+main()
