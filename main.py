8134d5d501919c3f053969f871a282ee83be037b
Clarets614 <cstock614@gmail.com>
2025-02-15 14:46:35 -0500

    report function finished

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ def main():
     file_text = get_book_text(path_to_file)
     letter_dictionary = count_characters(file_text)
     word_count = count_words(file_text)
+    listed_dict = list_it(letter_dictionary)
+    sorted_d = sort_dict(listed_dict)
+    report_alphabet(sorted_d, word_count)
     
-    print(f"word count:  {word_count}")
+    #print(f"word count:  {word_count}")
     
-    print(letter_dictionary)
+    #print(letter_dictionary)
+    
+    #print(report)
+    
 
 
 def get_book_text(path):
@@ -23,13 +29,27 @@ def count_words(contents):
     for w in words:
         count += 1        
     return count
-    
-def report_alphabet(dict, word_count):
+
+def list_it(dict):
+    return [{key: value} for key, value in dict.items()]    
+
+def sort_dict(list_dict):
+    sorted_dictionary = []
+    for pair in list_dict:
+        key = next(iter(pair))
+        if key.isalpha():
+            sorted_dictionary.append(pair)
+    #sorted_dictionary.sort(key=str.isalpha(), reverse=False)       
+    return sorted_dictionary
+
+def report_alphabet(sorted_dict, word_count):
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{word_count} words found in document")
-    
+    for letter in sorted_dict:
+        key = next(iter(letter))
+        print(f"The '{key}' character was found {letter[key]} times")
     print("--- End report ---")
-    pass
+    return
     
 def count_characters(input_string):
     lower_case = input_string.lower()
